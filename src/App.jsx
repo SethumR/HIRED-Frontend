dc31718ea5818c9a77338c19d79937f257847fe8
Sethum Ruberu <160596225+SethumR@users.noreply.github.com>
2024-12-17 04:19:26 +0530

    Imported Contact page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import Homepage from './Pages/HomePage';
 import Footer from './Components/Footer';
+import ContactPage from './Pages/ContactPage'; 
+
 
 const App = () => {
   return (
@@ -14,6 +16,8 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Homepage />} />
         </Routes>
+
+        <ContactPage/>
 
         <Footer />
       </div>
