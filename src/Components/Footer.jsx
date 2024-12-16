ed2590f31d119e26c24c2d4d79fa8f53402f69c3
Sethum Ruberu <160596225+SethumR@users.noreply.github.com>
2024-12-16 20:12:29 +0530

    Created Footer

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="py-6 bg-[#0E1526]">
+      <div className="container mx-auto">
+        <p className="text-center text-gray-400">
+          Copyright © 2024 HIRED
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
+
